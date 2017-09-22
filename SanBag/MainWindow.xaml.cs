891bc3fb70eb788eff566ba404281e6b20d9bbc5
nooperation <madbrahmin@gmail.com>
2017-09-21 20:12:52 -0400

    Adding texture viewer. Currently WIP

--- SanBag/MainWindow.xaml.cs
+++ SanBag/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using LibSanBag;
 using Microsoft.Win32;
+using ResourceUtils;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 
 namespace SanBag
@@ -30,6 +33,8 @@ namespace SanBag
             InitializeComponent();
 
             this.DataContext = this;
+
+            // HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Sansar  -> InstallLocation
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -116,6 +121,53 @@ namespace SanBag
             if (record != null)
             {
                 Clipboard.SetText($"https://sansar-asset-production.s3-us-west-2.amazonaws.com/{record.Name}");
+            }
+        }
+
+        private void dataGridContent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            imagePreview.Visibility = Visibility.Hidden;
+
+            try
+            {
+                var record = dataGridContent.SelectedItem as FileRecord;
+                if (record != null)
+                {
+                    var lowerName = record.Name.ToLower();
+                    if (lowerName.Contains("texture-resource") && lowerName.Contains("payload"))
+                    {
+                        using (var inStream = File.OpenRead(Backend.BagPath))
+                        {
+                            using (var outStream = new MemoryStream())
+                            {
+                                try
+                                {
+                                    record.Save(inStream, outStream);
+                                    var ddsBytes = OodleLz.DecompressTextureResource(outStream);
+                                    if (ddsBytes[0] == 'D' && ddsBytes[1] == 'D' && ddsBytes[2] == 'S')
+                                    {
+                                        var imageData = LibDDS.GetImageBytesFromDds(ddsBytes);
+
+                                        var image = new BitmapImage();
+                                        image.BeginInit();
+                                        image.StreamSource = new MemoryStream(imageData);
+                                        image.EndInit();
+
+                                        imagePreview.Source = image;
+                                        imagePreview.Visibility = Visibility.Visible;
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to view texture: " + ex.Message, "ERROR");
             }
         }
     }
