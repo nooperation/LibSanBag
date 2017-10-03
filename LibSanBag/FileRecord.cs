85202c121e2b50dfe6b9b04050d395c0bfc13391
nooperation <madbrahmin@gmail.com>
2017-10-02 22:47:39 -0400

    Now extracting additional information from file record name

--- LibSanBag/FileRecord.cs
+++ LibSanBag/FileRecord.cs
@@ -3,16 +3,57 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LibSanBag
 {
     public class FileRecord
     {
+        public enum ResourceType
+        {
+            AnimationBindingResource,
+            BankResource,
+            BlueprintResource,
+            ClusterDefinition,
+            EnvironmentResource,
+            GeometryResourceResource,
+            ModelMorphResource,
+            PickResource,
+            ShapeResource,
+            SoundResource,
+            TextureResource,
+            TextureSource,
+            WorldChunkDefinition,
+            WorldSource,
+            Unknown
+        }
+
+        public static Regex PatternRecord = new Regex(
+            @"(?<hash>[a-f0-9]{32})\." +
+                @"((?<image_name>.*\.png)|" +
+                @"(?<resource_type>[^\.]+)" +
+                    @"\.v(?<version_hash>[a-f0-9]{16})" +
+                    @"\.(?<content_type>[^\.]+)" +
+                    @"\.v(?<version_number>[0-9]+)" +
+                    @"\.(?<unknown_type>[a-zA-Z0-9]+)" +
+                @")",
+            RegexOptions.ExplicitCapture);
+
         public long Offset { get; set; }
         public uint Length { get; set; }
         public string Name { get; set; }
         public long TimestampNs { get; set; }
+
+        public string Hash { get; set; }
+        public string ImagePath { get; set; }
+        public ResourceType Type { get; set; }
+        public string VersionHash { get; set; }
+        public string ContentType { get; set; }
+        public int VersionNumber { get; set; }
+        public string Variants { get; set; }
+
+        public bool IsRawImage => ImagePath != null;
 
         public FileRecord(BinaryReader inStream)
         {
@@ -54,11 +95,60 @@ namespace LibSanBag
 
             var nameLength = inStream.ReadInt32();
             Name = new string(inStream.ReadChars(nameLength));
+
+            var match = PatternRecord.Match(Name);
+            Hash = match.Groups["hash"].Value;
+            var image_match = match.Groups["image_name"];
+
+            if (image_match.Success)
+            {
+                ImagePath = image_match.Value;
+            }
+            else
+            {
+                Type = GetResourceType(match.Groups["resource_type"].Value);
+                VersionHash = match.Groups["version_hash"].Value;
+                ContentType = match.Groups["content_type"].Value;
+                if (ContentType == "")
+                {
+                    Console.WriteLine("Butts");
+                }
+                VersionNumber = int.Parse(match.Groups["version_number"].Value);
+                Variants = match.Groups["unknown_type"].Value;
+            }
+        }
+
+        public static ResourceType GetResourceType(string resource_type_string)
+        {
+            switch (resource_type_string)
+            {
+                case "Bank-Resource": return ResourceType.BankResource;
+                case "Blueprint-Resource": return ResourceType.BlueprintResource;
+                case "Cluster-Definition": return ResourceType.ClusterDefinition;
+                case "Environment-Resource": return ResourceType.EnvironmentResource;
+                case "GeometryResource-Resource": return ResourceType.GeometryResourceResource;
+                case "hkaAnimationBinding-Resource": return ResourceType.AnimationBindingResource;
+                case "hknpShape-Resource": return ResourceType.ShapeResource;
+                case "ModelMorph-Resource": return ResourceType.ModelMorphResource;
+                case "Pick-Resource": return ResourceType.PickResource;
+                case "Sound-Resource": return ResourceType.SoundResource;
+                case "Texture-Resource": return ResourceType.TextureResource;
+                case "Texture-Source": return ResourceType.TextureSource;
+                case "WorldChunk-Definition": return ResourceType.WorldChunkDefinition;
+                case "World-Source": return ResourceType.WorldSource;
+            }
+
+            return ResourceType.Unknown;
         }
 
         public override string ToString()
         {
-            return $"{TimestampNs} - {Name} - {Length} bytes";
+            if (IsRawImage)
+            {
+                return Name;
+            }
+
+            return $"{TimestampNs} - {Hash} - {ContentType} - {Type} - {Variants} {Length} bytes";
         }
     }
 }
--- LibSanBag/Program.cs
+++ LibSanBag/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LibSanBag
@@ -19,20 +20,21 @@ namespace LibSanBag
             Directory.CreateDirectory(@".\in");
             Directory.CreateDirectory(@".\out");
 
-            using (var outStream = File.OpenWrite(@"Test.bag"))
-            {
-                Bag.Write(outStream, Directory.GetFiles(@".\in"), new TimeProvider());
-            }
+            //using (var outStream = File.OpenWrite(@"Test.bag"))
+            //{
+            //    Bag.Write(outStream, Directory.GetFiles(@".\in"), new TimeProvider());
+            //}
+            var pattern_record = new Regex(@"(?<hash>[a-z0-9]{32})\.(?<image_name>.*\.png)|(?<file_type>[^\.]+)\.v(?<version_hash>([a-z0-9]{16})\.(?<type>[^\.]+)\.v(?<version_number>[0-9]+)\.(?<unknown_type>[a-zA-Z0-9]+)");
 
-            using (var inStream = File.OpenRead(@"Test.bag"))
+            using (var inStream = File.OpenRead(@"C:\Users\Mario\AppData\Local\LindenLab\SansarClient\ClientAssetCacheLarge.bag"))
             {
-                var records = Bag.Read(inStream);
-
+                var records = Bag.Read(inStream).OrderBy(n => n.Name); ;
                 foreach (var fileRecord in records)
                 {
-                    using (var outStream = File.OpenWrite($@".\out\{fileRecord.Name}"))
+                    //using (var outStream = File.OpenWrite($@".\out\{fileRecord.Name}"))
                     {
-                        fileRecord.Save(inStream, outStream);
+                        var matches = pattern_record.Matches(fileRecord.Name);
+                        //fileRecord.Save(inStream, outStream);
                         Console.WriteLine(fileRecord);
                     }
                 }
