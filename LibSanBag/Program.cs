ca99ad7a2985771d4b1797903676793ae99c0a97
nooperation <madbrahmin@gmail.com>
2017-10-03 20:40:44 -0400

    Undoing program.cs changes

--- LibSanBag/Program.cs
+++ LibSanBag/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LibSanBag
@@ -20,21 +19,20 @@ namespace LibSanBag
             Directory.CreateDirectory(@".\in");
             Directory.CreateDirectory(@".\out");
 
-            //using (var outStream = File.OpenWrite(@"Test.bag"))
-            //{
-            //    Bag.Write(outStream, Directory.GetFiles(@".\in"), new TimeProvider());
-            //}
-            var pattern_record = new Regex(@"(?<hash>[a-z0-9]{32})\.(?<image_name>.*\.png)|(?<file_type>[^\.]+)\.v(?<version_hash>([a-z0-9]{16})\.(?<type>[^\.]+)\.v(?<version_number>[0-9]+)\.(?<unknown_type>[a-zA-Z0-9]+)");
-
-            using (var inStream = File.OpenRead(@"C:\Users\Mario\AppData\Local\LindenLab\SansarClient\ClientAssetCacheLarge.bag"))
+            using (var outStream = File.OpenWrite(@"Test.bag"))
             {
-                var records = Bag.Read(inStream).OrderBy(n => n.Name); ;
+                Bag.Write(outStream, Directory.GetFiles(@".\in"), new TimeProvider());
+            }
+
+            using (var inStream = File.OpenRead(@"Test.bag"))
+            {
+                var records = Bag.Read(inStream);
+
                 foreach (var fileRecord in records)
                 {
-                    //using (var outStream = File.OpenWrite($@".\out\{fileRecord.Name}"))
+                    using (var outStream = File.OpenWrite($@".\out\{fileRecord.Name}"))
                     {
-                        var matches = pattern_record.Matches(fileRecord.Name);
-                        //fileRecord.Save(inStream, outStream);
+                        fileRecord.Save(inStream, outStream);
                         Console.WriteLine(fileRecord);
                     }
                 }
@@ -42,3 +40,4 @@ namespace LibSanBag
         }
     }
 }
+
