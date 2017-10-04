95c61439b7d54dc924173f276df986d7d67658fc
nooperation <madbrahmin@gmail.com>
2017-10-03 20:35:44 -0400

    Fixes previous commit so tests pass again. Moved filename parsing to FileRecordInfo class

--- LibSanBag/FileRecord.cs
+++ LibSanBag/FileRecord.cs
@@ -10,50 +10,12 @@ namespace LibSanBag
 {
     public class FileRecord
     {
-        public enum ResourceType
-        {
-            AnimationBindingResource,
-            BankResource,
-            BlueprintResource,
-            ClusterDefinition,
-            EnvironmentResource,
-            GeometryResourceResource,
-            ModelMorphResource,
-            PickResource,
-            ShapeResource,
-            SoundResource,
-            TextureResource,
-            TextureSource,
-            WorldChunkDefinition,
-            WorldSource,
-            Unknown
-        }
+        public long Offset { get; private set; }
+        public uint Length { get; private set; }
+        public string Name { get; private set; }
+        public long TimestampNs { get; private set; }
 
-        public static Regex PatternRecord = new Regex(
-            @"(?<hash>[a-f0-9]{32})\." +
-                @"((?<image_name>.*\.png)|" +
-                @"(?<resource_type>[^\.]+)" +
-                    @"\.v(?<version_hash>[a-f0-9]{16})" +
-                    @"\.(?<content_type>[^\.]+)" +
-                    @"\.v(?<version_number>[0-9]+)" +
-                    @"\.(?<unknown_type>[a-zA-Z0-9]+)" +
-                @")",
-            RegexOptions.ExplicitCapture);
-
-        public long Offset { get; set; }
-        public uint Length { get; set; }
-        public string Name { get; set; }
-        public long TimestampNs { get; set; }
-
-        public string Hash { get; set; }
-        public string ImagePath { get; set; }
-        public ResourceType Type { get; set; }
-        public string VersionHash { get; set; }
-        public string ContentType { get; set; }
-        public int VersionNumber { get; set; }
-        public string Variants { get; set; }
-
-        public bool IsRawImage => ImagePath != null;
+        public FileRecordInfo Info { get; private set; }
 
         public FileRecord(BinaryReader inStream)
         {
@@ -96,59 +58,17 @@ namespace LibSanBag
             var nameLength = inStream.ReadInt32();
             Name = new string(inStream.ReadChars(nameLength));
 
-            var match = PatternRecord.Match(Name);
-            Hash = match.Groups["hash"].Value;
-            var image_match = match.Groups["image_name"];
-
-            if (image_match.Success)
-            {
-                ImagePath = image_match.Value;
-            }
-            else
-            {
-                Type = GetResourceType(match.Groups["resource_type"].Value);
-                VersionHash = match.Groups["version_hash"].Value;
-                ContentType = match.Groups["content_type"].Value;
-                if (ContentType == "")
-                {
-                    Console.WriteLine("Butts");
-                }
-                VersionNumber = int.Parse(match.Groups["version_number"].Value);
-                Variants = match.Groups["unknown_type"].Value;
-            }
-        }
-
-        public static ResourceType GetResourceType(string resource_type_string)
-        {
-            switch (resource_type_string)
-            {
-                case "Bank-Resource": return ResourceType.BankResource;
-                case "Blueprint-Resource": return ResourceType.BlueprintResource;
-                case "Cluster-Definition": return ResourceType.ClusterDefinition;
-                case "Environment-Resource": return ResourceType.EnvironmentResource;
-                case "GeometryResource-Resource": return ResourceType.GeometryResourceResource;
-                case "hkaAnimationBinding-Resource": return ResourceType.AnimationBindingResource;
-                case "hknpShape-Resource": return ResourceType.ShapeResource;
-                case "ModelMorph-Resource": return ResourceType.ModelMorphResource;
-                case "Pick-Resource": return ResourceType.PickResource;
-                case "Sound-Resource": return ResourceType.SoundResource;
-                case "Texture-Resource": return ResourceType.TextureResource;
-                case "Texture-Source": return ResourceType.TextureSource;
-                case "WorldChunk-Definition": return ResourceType.WorldChunkDefinition;
-                case "World-Source": return ResourceType.WorldSource;
-            }
-
-            return ResourceType.Unknown;
+            Info = FileRecordInfo.Create(Name);
         }
 
         public override string ToString()
         {
-            if (IsRawImage)
+            if (Info == null || Info.IsRawImage)
             {
                 return Name;
             }
 
-            return $"{TimestampNs} - {Hash} - {ContentType} - {Type} - {Variants} {Length} bytes";
+            return $"{TimestampNs} - {Info.Hash} - {Info.ContentType} - {Info.Type} - {Info.Variants} {Length} bytes";
         }
     }
 }
