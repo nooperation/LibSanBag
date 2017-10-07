1f5a0ce1bbebf072b7e7274076931b8416d08911
nooperation <madbrahmin@gmail.com>
2017-10-06 20:49:27 -0400

    Added preview image functionality

--- SanBag/ViewModels/TextureResourceViewModel.cs
+++ SanBag/ViewModels/TextureResourceViewModel.cs
@@ -1,17 +1,100 @@
-﻿using System;
+﻿using LibSanBag;
+using ResourceUtils;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace SanBag.ViewModels
 {
-    public class TextureResourceViewModel : GenericBagViewModel
+    public class TextureResourceViewModel : GenericBagViewModel, INotifyPropertyChanged
     {
+        private FileRecord _selectedRecord;
+        public FileRecord SelectedRecord
+        {
+            get => _selectedRecord;
+            set
+            {
+                _selectedRecord = value;
+                UpdatePreviewImage();
+                OnPropertyChanged();
+            }
+        }
+
+        private BitmapImage _currentPreview;
+        public BitmapImage PreviewImage
+        {
+            get => _currentPreview;
+            set
+            {
+                _currentPreview = value;
+                OnPropertyChanged();
+            }
+        }
+
         public TextureResourceViewModel(MainViewModel parentViewModel)
             : base(parentViewModel)
         {
-            
+
+        }
+
+        private void UpdatePreviewImage()
+        {
+            try
+            {
+                if (SelectedRecord == null)
+                {
+                    return;
+                }
+
+                if (SelectedRecord?.Info.Type != FileRecordInfo.ResourceType.TextureResource || SelectedRecord?.Info.ContentType != "payload")
+                {
+                    return;
+                }
+
+                using (var inStream = File.OpenRead(ParentViewModel.BagPath))
+                {
+                    using (var outStream = new MemoryStream())
+                    {
+                        try
+                        {
+                            SelectedRecord.Save(inStream, outStream);
+                            var ddsBytes = OodleLz.DecompressTextureResource(outStream);
+                            if (ddsBytes[0] == 'D' && ddsBytes[1] == 'D' && ddsBytes[2] == 'S')
+                            {
+                                var imageData = LibDDS.GetImageBytesFromDds(ddsBytes);
+
+                                var image = new BitmapImage();
+                                image.BeginInit();
+                                image.StreamSource = new MemoryStream(imageData);
+                                image.EndInit();
+
+                                PreviewImage = image;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            PreviewImage = new BitmapImage();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to view texture: " + ex.Message, "ERROR");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
