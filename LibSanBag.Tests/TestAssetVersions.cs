cfefff80a68b66c4d7371a207d57cd9d6e7b0378
nooperation <madbrahmin@gmail.com>
2017-11-07 20:01:56 -0500

    Added reverse lookup to find resource type from version string. Updated AssetVersions tests

--- LibSanBag.Tests/TestAssetVersions.cs
+++ LibSanBag.Tests/TestAssetVersions.cs
@@ -11,7 +11,7 @@ namespace LibSanBag.Tests
     class TestAssetVersions
     {
         [Test]
-        public void TestCreateEmptyBag()
+        public void TestGetResourceVersions()
         {
             var enums = Enum.GetValues(typeof(FileRecordInfo.ResourceType));
             foreach (var item in enums)
@@ -21,6 +21,29 @@ namespace LibSanBag.Tests
                     AssetVersions.GetResourceVersions((FileRecordInfo.ResourceType) item);
                 });
             }
+        }
+
+        [Test]
+        public void TestGetResourceTypeFromVersion()
+        {
+            var enums = Enum.GetValues(typeof(FileRecordInfo.ResourceType));
+            foreach (var item in enums)
+            {
+                var expectedResourceType = (FileRecordInfo.ResourceType)item;
+                var versions = AssetVersions.GetResourceVersions(expectedResourceType);
+                if (versions.Count > 0)
+                {
+                    var resourceType = AssetVersions.GetResourceTypeFromVersion(versions[0]);
+                    Assert.AreEqual(expectedResourceType, resourceType);
+                }
+            }
+        }
+
+        [Test]
+        public void TestGetResourceTypeFromVersionUnknown()
+        {
+            var resourceType = AssetVersions.GetResourceTypeFromVersion("UnknownVersion");
+            Assert.AreEqual(FileRecordInfo.ResourceType.Unknown, resourceType);
         }
     }
 }
