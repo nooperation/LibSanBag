021287df751bc938c75889c99bd2c2660c0370ab
nooperation <madbrahmin@gmail.com>
2017-12-08 19:43:14 -0500

    Missed ManifestResource changes. Fixed file parser so it can correctly read uncompressed data

--- LibSanBag/FileResources/BaseFileResource.cs
+++ LibSanBag/FileResources/BaseFileResource.cs
@@ -16,7 +16,20 @@ namespace LibSanBag.FileResources
             using (var compressedStream = new MemoryStream())
             {
                 fileRecord.Save(sourceStream, compressedStream);
-                decompressedBytes = OodleLz.DecompressResource(compressedStream);
+
+                try
+                {
+                    decompressedBytes = OodleLz.DecompressResource(compressedStream);
+                }
+                catch (Exception)
+                {
+                    compressedStream.Seek(0, SeekOrigin.Begin);
+                    using (var decompressedStream = new MemoryStream())
+                    {
+                        compressedStream.CopyTo(decompressedStream);
+                        decompressedBytes = decompressedStream.ToArray();
+                    }
+                }
             }
 
             InitFromRawDecompressed(decompressedBytes);
@@ -24,7 +37,20 @@ namespace LibSanBag.FileResources
 
         public void InitFromStream(Stream compressedStream)
         {
-            var decompressedBytes = OodleLz.DecompressResource(compressedStream);
+            byte[] decompressedBytes = null;
+            try
+            {
+                decompressedBytes = OodleLz.DecompressResource(compressedStream);
+            }
+            catch (Exception)
+            {
+                compressedStream.Seek(0, SeekOrigin.Begin);
+                using (var decompressedStream = new MemoryStream())
+                {
+                    compressedStream.CopyTo(decompressedStream);
+                    decompressedBytes = decompressedStream.ToArray();
+                }
+            }
 
             InitFromRawDecompressed(decompressedBytes);
         }
@@ -35,7 +61,19 @@ namespace LibSanBag.FileResources
 
             using (var compressedStream = new MemoryStream(compressedBytes))
             {
-                decompressedBytes = OodleLz.DecompressResource(compressedStream);
+                try
+                {
+                    decompressedBytes = OodleLz.DecompressResource(compressedStream);
+                }
+                catch (Exception)
+                {
+                    compressedStream.Seek(0, SeekOrigin.Begin);
+                    using (var decompressedStream = new MemoryStream())
+                    {
+                        compressedStream.CopyTo(decompressedStream);
+                        decompressedBytes = decompressedStream.ToArray();
+                    }
+                }
             }
 
             InitFromRawDecompressed(decompressedBytes);
