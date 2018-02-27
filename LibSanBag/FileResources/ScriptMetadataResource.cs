39ce0688cfb5d36ee4c8fc0d15f13668363f5b85
nooperation <madbrahmin@gmail.com>
2018-02-26 19:22:50 -0500

    More hacking at ScriptMetadataResource. This one is bizarre

--- LibSanBag/FileResources/ScriptMetadataResource.cs
+++ LibSanBag/FileResources/ScriptMetadataResource.cs
@@ -13,8 +13,10 @@ namespace LibSanBag.FileResources
         {
             public string Name { get; set; }
             public string Type { get; set; }
+            public List<KeyValuePair<string, string>> Attributes { get; set; }
         }
 
+        public string AssemblyName { get; set; }
         public string Warnings { get; set; }
         public List<PropertyEntry> Properties { get; set; }
         public List<KeyValuePair<string, string>> Strings { get; set; }
@@ -23,14 +25,17 @@ namespace LibSanBag.FileResources
         {
             switch (version)
             {
+
+                case "0a316ea155e30eda":
+                    return new ScriptMetadataResourceV1();
                 case "67df52a55a73f7d3":
                 default:
-                    return new ScriptMetadataResourceV1();
+                    return new ScriptMetadataResourceV3();
             }
         }
     }
 
-    public class ScriptMetadataResourceV1 : ScriptMetadataResource
+    public class ScriptMetadataResourceV3 : ScriptMetadataResource
     {
         public override bool IsCompressed => true;
 
@@ -55,23 +60,72 @@ namespace LibSanBag.FileResources
 
                 for (var i = 0; i < propertyCount; ++i)
                 {
+                    var attributes = new List<KeyValuePair<string, string>>();
+
                     var nameLength = decompressedStream.ReadInt32();
                     var name= new string(decompressedStream.ReadChars(nameLength));
 
                     var typeLength = decompressedStream.ReadInt32();
                     var type = new string(decompressedStream.ReadChars(typeLength));
 
-                    var unknownF = decompressedStream.ReadInt32();
-                    var unknownG = decompressedStream.ReadInt32();
-                    if (unknownG == 1)
+                    var shouldCheckForAttributes = true;
+
+                    var typeCode = decompressedStream.ReadInt32();
+                    if (typeCode == 0x400)
                     {
-                        var unknownH = decompressedStream.ReadInt32();
+                        var unknownG = decompressedStream.ReadInt32();
+                        shouldCheckForAttributes = unknownG == 1;
+                    }
+
+                    if(shouldCheckForAttributes)
+                    {
+                        var numAttributes = decompressedStream.ReadInt32();
+                        if (numAttributes > 5)
+                        {
+                            var attributeKeyLength = numAttributes;
+                            var attributeKey = new string(decompressedStream.ReadChars(attributeKeyLength));
+
+                            var attributeValueCode = decompressedStream.ReadInt32();
+                            var attributeValueLength = decompressedStream.ReadInt32();
+                            var attributeValue = new string(decompressedStream.ReadChars(attributeValueLength));
+
+                            attributes.Add(new KeyValuePair<string, string>(attributeKey, attributeValue));
+                        }
+                        else
+                        {
+                            if (typeCode == 0x400)
+                            {
+                                var unknownZ = decompressedStream.ReadInt32();
+                            }
+
+                            for (var attributeIndex = 0; attributeIndex < numAttributes; attributeIndex++)
+                            {
+                                var attributeKeyLength = decompressedStream.ReadInt32();
+                                var attributeKey = new string(decompressedStream.ReadChars(attributeKeyLength));
+                                var attributeValueCode = decompressedStream.ReadInt32();
+
+                                if (attributeValueCode == 0x400)
+                                {
+                                    var attributeValueLength = decompressedStream.ReadInt32();
+                                    var attributeValue = new string(decompressedStream.ReadChars(attributeValueLength));
+
+                                    attributes.Add(new KeyValuePair<string, string>(attributeKey, attributeValue));
+                                }
+                                else
+                                {
+                                    var attributeValue = decompressedStream.ReadUInt32().ToString();
+                                    attributes.Add(new KeyValuePair<string, string>(attributeKey, attributeValue));
+                                    decompressedStream.ReadUInt32();
+                                }
+                            }
+                        }
                     }
 
                     Properties.Add(new PropertyEntry()
                     {
                         Name = name,
-                        Type = type
+                        Type = type,
+                        Attributes = attributes
                     });
                 }
 
@@ -80,7 +134,7 @@ namespace LibSanBag.FileResources
                 var stringCount = decompressedStream.ReadInt32();
                 Strings = new List<KeyValuePair<string, string>>(stringCount);
 
-                for (var i = 0; i < stringCount; ++i)
+                for (var stringIndex = 0; stringIndex < stringCount; ++stringIndex)
                 {
                     var keyLength = decompressedStream.ReadInt32();
                     var key = new string(decompressedStream.ReadChars(keyLength));
@@ -89,6 +143,35 @@ namespace LibSanBag.FileResources
                     var value = new string(decompressedStream.ReadChars(valueLength));
 
                     Strings.Add(new KeyValuePair<string, string>(key, value));
+                }
+            }
+        }
+    }
+
+    public class ScriptMetadataResourceV1 : ScriptMetadataResource
+    {
+        public override bool IsCompressed => true;
+
+        public override void InitFromRawDecompressed(byte[] decompressedBytes)
+        {
+            using (var decompressedStream = new BinaryReader(new MemoryStream(decompressedBytes)))
+            {
+                var propertyCount = decompressedStream.ReadInt32();
+                Properties = new List<PropertyEntry>(propertyCount);
+
+                for (var i = 0; i < propertyCount; ++i)
+                {
+                    var nameLength = decompressedStream.ReadInt32();
+                    var name = new string(decompressedStream.ReadChars(nameLength));
+
+                    var typeLength = decompressedStream.ReadInt32();
+                    var type = new string(decompressedStream.ReadChars(typeLength));
+
+                    Properties.Add(new PropertyEntry()
+                    {
+                        Name = name,
+                        Type = type
+                    });
                 }
             }
         }
