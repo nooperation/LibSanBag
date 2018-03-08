ca8106abed9ed4d3522ad64f6f101063e430f9d0
nooperation <madbrahmin@gmail.com>
2018-03-07 20:58:06 -0500

    Adding ScriptMetadata resource scamples

---
+++ LibSanBag.Tests/Samples/Resources/ScriptMetadata/Props1.cs
@@ -0,0 +1,44 @@
+﻿using Sansar.Simulation;
+using System;
+using System.Collections.Generic;
+using Sansar.Script;
+using Sansar;
+
+// This is an example script used for research.
+public class ExampleScript : SceneObjectScript
+{
+    [DefaultValue(42.42)]
+    [Range(1.234, 40.42)]
+    [DisplayName("Example Display Name")]
+    public readonly double ExampleDoubleRangeAll;
+            
+    [DefaultValue(42.42)]
+    [DisplayName("Example Display Name")]
+    public readonly double ExampleDoubleDefault42Display;
+    
+    [DefaultValue(42.42)]
+    public readonly double ExampleDoubleDefault42;
+    
+    [DisplayName("Example Display Name")]
+    public readonly double ExampleDoubleDisplay;
+
+    public readonly double ExampleDouble;
+
+    [DefaultValue("<1.2,3.4,5.6>")]
+    [DisplayName("Example Display Name")]
+    public Sansar.Vector ExampleVectorDefaultDisplay;
+
+    [DefaultValue("<1.2,3.4,5.6>")]
+    public Sansar.Vector ExampleVectorDefault;
+
+    [DisplayName("Example Display Name")] 
+    public Sansar.Vector ExampleVectorDisplay;
+
+    public Sansar.Vector ExampleVector;
+
+    public override void Init()
+    {
+        Log.Write("Simple example script for research purposes.");
+        float warning = (float)42;
+    }
+}
---
+++ LibSanBag.Tests/Samples/Resources/ScriptMetadata/Props2.cs
@@ -0,0 +1,52 @@
+﻿using Sansar.Simulation;
+using System;
+using Sansar.Script;
+using Sansar;
+
+// This is an example script used for research.
+public class ExampleScript : SceneObjectScript
+{
+    [DefaultValue("Example Default Value")]
+    [DisplayName("Example Display Name")]
+    public readonly string ExampleStringPropertyDefaultDisplay;
+    
+    [DisplayName("Example Display Name")]
+    public readonly string ExampleStringPropertyDisplay;
+    
+    [DefaultValue("Example Default Value")]
+    public readonly string ExampleStringPropertyDefault;
+
+    public readonly string ExampleStringProperty;
+
+    [DefaultValue(42)]
+    [DisplayName("Example Display Name")]
+    public readonly int ExampleIntPropertyDefaultDisplay;
+    
+    [DefaultValue(42)]
+    public readonly int ExampleIntPropertyDefault;
+    
+    [DisplayName("Example Display Name")]
+    public readonly int ExampleIntPropertyDisplay;
+    
+    public readonly int ExampleIntProperty;
+        
+    [DefaultValue(42.0)]
+    [DisplayName("Example Display Name")]
+    public readonly double ExampleDoublePropertyDefault42Display;
+    
+    [DefaultValue(42.0)]
+    public readonly double ExampleDoublePropertyDefault42;
+    
+    [DisplayName("Example Display Name")]
+    public readonly double ExampleDoublePropertyDisplay;
+
+    public readonly double ExampleDoubleProperty;
+    
+    public readonly int ExampleIntPropertyNoDefault;
+    
+    public override void Init()
+    {
+        Log.Write("Simple example script for research purposes.");
+        float warning = (float)ExampleIntPropertyDisplay;
+    }
+}
