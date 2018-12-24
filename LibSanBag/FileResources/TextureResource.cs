10e43ec242654db60516c498a363544554ed5f9b
nooperation <madbrahmin@gmail.com>
2018-12-24 00:52:52 -0500

    Starting to look into the v2 texture format with progressive/streamed mips

--- LibSanBag/FileResources/TextureResource.cs
+++ LibSanBag/FileResources/TextureResource.cs
@@ -26,6 +26,11 @@ namespace LibSanBag.FileResources
         /// </summary>
         public byte[] CompressedTextureBytes { get; set; }
 
+        public int UnknownA { get; set; }
+        public List<string> StreamedMips { get; set; } = new List<string>();
+        public int UnknownB { get; set; }
+        public int UnknownC { get; set; }
+
         public static TextureResource Create(string version = "")
         {
             switch (version)
@@ -33,8 +38,12 @@ namespace LibSanBag.FileResources
                 case "9a8d4bbd19b4cd55":
                     return new TextureResource_9a8d4bbd19b4cd55();
                 case "bfc630a1f9234ffd":
-                default:
+                case "30cfeb60ba0b3d8c":
+                case "f1b6b03a0c99d181":
+                case "1":
                     return new TextureResource_bfc630a1f9234ffd();
+                default:
+                    return new TextureResource_v2();
             }
         }
 
@@ -144,6 +153,88 @@ namespace LibSanBag.FileResources
                 else
                 {
                     throw new Exception("Could not find CRN or DDS header in decompressed data");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts this texture to a different resolution, codec, or format
+        /// </summary>
+        /// <param name="codec">Type of image to convert this texture to</param>
+        /// <param name="width">Width to resize image to. May not be available. Width of 0 preserves the original width.</param>
+        /// <param name="height">Height to resize image to. May not be available. Height of 0 preserves the original height.</param>
+        /// <returns>Converted image bytes</returns>
+        public override byte[] ConvertTo(TextureType codec, int width = 0, int height = 0)
+        {
+            if (SourceType == TextureType.CRN)
+            {
+                if (codec == TextureType.CRN)
+                {
+                    return CompressedTextureBytes;
+                }
+
+                var crnCodec = GetCrnTextureType(codec);
+                return LibCRN.GetImageBytesFromCRN(CompressedTextureBytes, crnCodec);
+            }
+            else
+            {
+                if (codec == TextureType.DDS)
+                {
+                    return CompressedTextureBytes;
+                }
+
+                var ddsCodec = GetDdsTextureType(codec);
+                return LibDDS.GetImageBytesFromDds(CompressedTextureBytes, width, height, ddsCodec);
+            }
+        }
+    }
+
+    public class TextureResource_v2 : TextureResource
+    {
+        public override bool IsCompressed => true;
+
+        public override void InitFromRawDecompressed(byte[] decompressedBytes)
+        {
+            using (var br = new BinaryReader(new MemoryStream(decompressedBytes)))
+            {
+                var numBytes = br.ReadInt32();
+                var textureBytes = br.ReadBytes(numBytes);
+                if (textureBytes[0] == 'H' && textureBytes[1] == 'x')
+                {
+                    SourceType = TextureType.CRN;
+                    CompressedTextureBytes = textureBytes;
+                }
+                else if (textureBytes[0] == 'D' && textureBytes[1] == 'D' && textureBytes[2] == 'S')
+                {
+                    SourceType = TextureType.DDS;
+                    CompressedTextureBytes = textureBytes;
+                }
+                else
+                {
+                    throw new Exception("Could not find CRN or DDS header in decompressed data");
+                }
+
+                var numBytesRemaining = br.BaseStream.Length - br.BaseStream.Position;
+                if(numBytesRemaining >= 4) 
+                {
+                    UnknownA = br.ReadInt32();
+                    var numAdditionalMips = br.ReadInt32();
+                
+                    if(numAdditionalMips > 0)
+                    {
+                        UnknownB = br.ReadInt32();
+                        UnknownC = br.ReadInt32();
+
+                        for(int i = 0; i < numAdditionalMips; ++i)
+                        {
+                            var hashLower = br.ReadUInt64();
+                            var hashUpper = br.ReadUInt64();
+
+                            var hashFinal = $"{hashLower:x16}{hashUpper:x16}";
+                            Console.WriteLine($"http://sansar-asset-production.s3-us-west-2.amazonaws.com/{hashFinal}.TextureMip-Resource.v1.payload.v0.noVariants");
+                            StreamedMips.Add(hashFinal);
+                        }
+                    }
                 }
             }
         }
