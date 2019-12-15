10cd34375738ad148324a99436d14c8d117e6c7e
nooperation <madbrahmin@gmail.com>
2019-12-15 18:19:07 -0500

    Added PickResource Parser Added TextureSource Parser

---
+++ LibSanBag/FileResources/PickResource.cs
@@ -0,0 +1,51 @@
+﻿using LibSanBag;
+using LibSanBag.ResourceUtils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSanBag.FileResources
+{
+    public class PickResource : BaseFileResource
+    {
+        public override bool IsCompressed => true;
+
+        public static PickResource Create(string version = "")
+        {
+            return new PickResource();
+        }
+
+        public class Pick
+        {
+            public uint Version { get; internal set; }
+            public uint MeshBufferLength { get; internal set; }
+
+            [JsonIgnore]
+            public byte[] HavokShape { get; internal set; }
+        }
+
+        private Pick Read_PickResource(BinaryReader reader)
+        {
+            var result = new Pick();
+
+            result.Version = ReadVersion(reader, 1, 0x1411E1F50);
+            result.MeshBufferLength = reader.ReadUInt32();
+            result.HavokShape = reader.ReadBytes((int)result.MeshBufferLength);
+
+            return result;
+        }
+
+        public Pick Resource { get; set; }
+        public override void InitFromRawDecompressed(byte[] decompressedBytes)
+        {
+            using (var reader = new BinaryReader(new MemoryStream(decompressedBytes)))
+            {
+                this.Resource = Read_PickResource(reader);
+            }
+        }
+    }
+}
---
+++ LibSanBag/FileResources/TextureSource.cs
@@ -0,0 +1,104 @@
+﻿using LibSanBag;
+using LibSanBag.ResourceUtils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSanBag.FileResources
+{
+    public class TextureSource : BaseFileResource
+    {
+        public override bool IsCompressed => true;
+
+        public static TextureSource Create(string version = "")
+        {
+            return new TextureSource();
+        }
+
+        public class Thumbnail
+        {
+            public uint Version { get; internal set; }
+            [JsonIgnore]
+            public byte[] Data { get; internal set; }
+            public ulong Width { get; internal set; }
+            public ulong Height { get; internal set; }
+            public string Format { get; internal set; }
+            public List<float> AverageColor { get; internal set; }
+        }
+        private Thumbnail Read_Thumbnail(BinaryReader reader)
+        {
+            var result = new Thumbnail();
+
+            result.Version = ReadVersion(reader, 4, 0x1411C4E30);
+            result.Data = Read_Array(reader);
+            result.Width = reader.ReadUInt64();
+            result.Height = reader.ReadUInt64();
+
+            if(result.Version < 4)
+            {
+                result.Format = ReadString(reader);
+            }
+
+            if(result.Version >= 2)
+            {
+                result.AverageColor = ReadVectorF(reader, 4);
+            }
+
+            return result;
+        }
+
+        public class Texture
+        {
+            public uint Version { get; internal set; }
+            public string Name { get; internal set; }
+            public string TextureId { get; internal set; }
+            public Thumbnail Thumbnail { get; internal set; }
+            public ulong Width { get; internal set; }
+            public ulong Height { get; internal set; }
+            public uint OpacityFlags { get; internal set; }
+            public bool SemiOpaque { get; internal set; }
+            public string Format { get; internal set; }
+        }
+        private Texture Read_TextureSource(BinaryReader reader)
+        {
+            var result = new Texture();
+
+            result.Version = ReadVersion(reader, 3, 0x141114210);
+
+            if(result.Version < 2)
+            {
+                result.Name = ReadString(reader);
+            }
+
+            result.TextureId = ReadUUID(reader);
+            result.Thumbnail = Read_Thumbnail(reader);
+            result.Width = reader.ReadUInt64();
+            result.Height = reader.ReadUInt64();
+
+            if(result.Version >= 3)
+            {
+                result.OpacityFlags = reader.ReadUInt32();
+            }
+            else
+            {
+                result.SemiOpaque = reader.ReadBoolean();
+            }
+
+            result.Format = ReadString(reader);
+            return result;
+        }
+
+        public Texture Resource { get; set; }
+        public override void InitFromRawDecompressed(byte[] decompressedBytes)
+        {
+            using (var reader = new BinaryReader(new MemoryStream(decompressedBytes)))
+            {
+                this.Resource = Read_TextureSource(reader);
+            }
+        }
+    }
+}
